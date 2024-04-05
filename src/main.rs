7c2aed6810ce021b1b176196da6a0c1046e6a08a
jackr-m <jack7nugget@gmail.com>
2024-04-05 11:43:14 -0700

    Fixed UART behavior (previously would run out of string memory)

--- src/main.rs
+++ src/main.rs
@@ -26,22 +26,19 @@ async fn main(_spawner: Spawner) {
 
     // Variables
     let mut loop_counter: u16 = 0; // Create loop counter
-    const MAX_STRING_LENGTH: usize = "LED has blinked  times".len() + 5; // Define max string length
+    const MAX_STRING_LENGTH: usize = "LED has blinked  times\n".len() + 5; // Define max string length
     let mut msg: String<MAX_STRING_LENGTH> = String::new(); // Create blank string for UART print
     
     // info!("Starting LED Blinking..."); // debug way to do it
-    // core::writeln!(&mut msg, "Starting LED Blinking...").unwrap();
-    msg.push_str("Starting LED Blinking...\n").unwrap();
-    // usart.blocking_write(msg.as_bytes()).unwrap();
+    core::writeln!(&mut msg, "Starting LED Blinking...").unwrap();
+    usart.blocking_write(msg.as_bytes()).unwrap();
     
     loop {
         led.set_high();
         // info!("LED Has Blinked {} times", loop_counter); // debug way to do it
-        let remaining_capacity = msg.capacity() - msg.len();
-        if remaining_capacity >= MAX_STRING_LENGTH {
-            core::writeln!(&mut msg, "LED has blinked {} times", loop_counter).unwrap();
-            usart.blocking_write(msg.as_bytes()).unwrap();
-        }
+        msg.clear(); // NEED to clear the string or else it keeps getting longer
+        core::writeln!(&mut msg, "LED has blinked {} times", loop_counter).unwrap();
+        usart.blocking_write(msg.as_bytes()).unwrap();
         
         Timer::after_millis(200).await;
         led.set_low();
